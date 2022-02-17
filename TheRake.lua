a6f03c46a5bbf62dc09572fb98c48966b6eece52
irlng <96001120+irlng@users.noreply.github.com>
2022-02-17 19:17:05 +0700

    Add files via upload

---
+++ TheRake.lua
@@ -0,0 +1,18 @@
+if wait(.5) then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/irlng/TheRakeRemastered/main/FlareGun.lua"))()
+end
+
+if wait() then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/irlng/TheRakeRemastered/main/Places%20Esp.lua"))()
+end
+
+if wait() then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/irlng/TheRakeRemastered/main/Rake%20Esp.lua"))()
+end
+
+if wait() then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/irlng/TheRakeRemastered/main/Scrap%20Esp.lua"))()
+end
+
+
+
